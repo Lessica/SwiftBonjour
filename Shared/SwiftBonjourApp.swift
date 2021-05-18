8cb0941df356ffbf1bb61ca0895bd72faff7003f
82Flex <82flex@gmail.com>
2021-05-18 22:21:06 +0800

    Initial Commit

---
+++ Shared/SwiftBonjourApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBonjourApp.swift
+//  Shared
+//
+//  Created by Rachel on 2021/5/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBonjourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
